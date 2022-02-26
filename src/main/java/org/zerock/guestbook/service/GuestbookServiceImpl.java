f903eb91202a3b27ba88b398c3016c926e2d7234
jyyoun1022 <jyyoun1022@naver.com>
2022-02-26 18:57:44 +0900

    검색처리

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -18,6 +18,8 @@ import org.zerock.guestbook.repository.GuestBookRepository;
 import java.util.Optional;
 import java.util.function.Function;
 
+import static org.zerock.guestbook.entity.QGuestbook.*;
+
 
 @Service
 @Log4j2
@@ -45,9 +47,13 @@ private final GuestBookRepository repository;
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
 
         BooleanBuilder booleanBuilder = getSearch(requestDTO); // 검색 조건 처리
+        //페이징,정렬 = findall // 반환타입 page<entity>
+        //클라이언트로부터 온 요청 결과값 = jpa에서 나온 결과값(entity)
+        //pageable 타입 파라미터 findall에 넣으면 쿼리가 실행
 
+        //Querydsl 사용(검색결과)후 리스트 페이지
         Page<Guestbook> result = repository.findAll(booleanBuilder,pageable); // Querydsl 사용
-
+        //Guestbook(entity)를 넣으면 GuestbookDTO(dto)로 나온다.
         Function<Guestbook,GuestbookDTO> fn = (entity -> entityToDto(entity));
 
         return new PageResultDTO<>(result,fn);
@@ -86,38 +92,38 @@ private final GuestBookRepository repository;
      */
     private BooleanBuilder getSearch(PageRequestDTO requestDTO) {
 
-        String type = requestDTO.getType();
+        String type = requestDTO.getType(); //검색조건 = title, content, writer
 
-        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();   //조건문 담을 컨테이너 생성(BooleanBuilder는 where조건문에 들어가는 조건들을 넣어주는 컨테이너)
 
-        QGuestbook qGuestbook = QGuestbook.guestbook;
+//        QGuestbook qGuestbook = QGuestbook.guestbook;   //큐 도메인 생성    //QGuestbook을 static import처리
 
-        String keyword = requestDTO.getKeyword();
+        String keyword = requestDTO.getKeyword();   //검색어
 
-        BooleanExpression expression = qGuestbook.gno.gt(0L);   //gno > 0 조건만 생성
+        BooleanExpression expression = guestbook.gno.gt(0L);   //gno > 0 조건만 생성
 
-        booleanBuilder.and(expression);
+        booleanBuilder.and(expression); //건테이너에 조건문 담기
 
-        if(type==null || type.trim().length()==0){return booleanBuilder;    //검색조건이 없는 경우
+        if(type==null || type.trim().length()==0){return booleanBuilder;    //조건문이 null 이거나 0이면
     }
 
 
         // 검색 조건 작성
         // PageRequestDTO 를 파라미터로 받아 검색조건이 있는 경우 conditionBuilder 변수를 생성해 각 검색조건을 or로 연결해 처리한다
-        BooleanBuilder conditionBuilder = new BooleanBuilder();
+        BooleanBuilder conditionBuilder = new BooleanBuilder(); //컨테이너2
 
-        if(type.contains("t")){
-            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        if(type.contains("t")){ //제목으로 검색   / type=제목,작성자,내용 에 keyword로 검색
+            conditionBuilder.or(guestbook.title.contains(keyword));    //
         }
-        if(type.contains("c")){
-            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        if(type.contains("c")){ //내용으로 검색
+            conditionBuilder.or(guestbook.content.contains(keyword));
         }
-        if(type.contains("w")){
-            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        if(type.contains("w")){ //작성자로 검색
+            conditionBuilder.or(guestbook.writer.contains(keyword));
         }
 
         // 모든 조건 통합
-        booleanBuilder.and(conditionBuilder);
+        booleanBuilder.and(conditionBuilder);//컨테이너1+컨테이너2
 
         return booleanBuilder;
     }
--- src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -57,5 +57,29 @@ public class GuestbookServiceTests {
         System.out.println("======================================");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
+    @Test
+    void testSearch(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("c")  //검색조건 t,c,w,tc,tcw
+                .keyword("C")  //검색 키워드
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+
+        System.out.println("================================");
+
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("===============================");
+        resultDTO.getPageList().forEach(i-> System.out.println(i));
+    }
 
 }
